7c9aece049b71d53212197f6fb4c9095b6061212
safaabe <bahassounesafa66@gmail.com>
2024-05-04 23:50:16 +0200

    commit

--- dashboard.php
+++ dashboard.php
@@ -9,11 +9,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.0.7/css/boxicons.min.css">
 
     <style>
-        * {
+        body {
             margin: 0;
             padding: 0;
-            box-sizing: border-box;
-            font-family: "Rubik", sans-serif; /* Applying Rubik font to all elements */
+            background-color: #eee;
+            /* Red background for the entire page */
+            font-family: "Rubik", sans-serif;
+            /* Applying Rubik font to all elements */
         }
 
         .header {
@@ -31,7 +33,7 @@
             padding-top: 60px;
             background-color: #102C57;
             height: 100vh;
-            width: 170px;
+            width: 200px;
             position: fixed;
             top: 0;
             left: 0;
@@ -56,28 +58,85 @@
             color: #fff;
             padding: 10px;
             display: block;
-            font-size:15px ;
+            font-size: 15px;
         }
-        ul li i{
+
+        ul li i {
             margin-right: 10px;
+        }
+
+        .cartes {
+            display: flex;
+            flex-direction: row;
+            margin-left: 250px;
+            margin-top: 150px;
+        }
+
+        .card {
+            border: 1px solid transparent;
+            border-radius: 10px;
+            padding: 20px;
+            margin-right: 10px;
+            height: 80px;
+            width: 250px;
+            background-color: #fff;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+
+        .card h4 {
+            position: absolute;
+            top: 150px
+        }
+
+        .client {
+            border: 1px solid black;
+            /* Adding a border to make it visible */
+            width: 100px;
+            height: 250px;
+            background-color: #fff;
+            /* Adding a background color */
         }
     </style>
 </head>
 
 <body>
-    <div class="header"></div>
+    <div class="container">
+        <div class="header"></div>
 
-    <div class="sidebar">
-        <div class="logo"><img src="img/sinmatexlogo.png" alt="simatex"></div>
-        <ul>
-            <li><a href=""><i class='bx bx-line-chart' style='color:#ffffff' ></i> Dashboard</a></li>
-            <li><a href=""><i class='bx bxs-group' style='color:#ffffff' ></i>Clients</a></li>
-            <li><a href=""><i class='bx bx-box'></i>Commandes</a></li>
-            <li><a href=""><i class='bx bxs-pie-chart-alt' style='color:#ffffff' ></i>Fournisseurs</a></li>
-            <li><a href=""><i class='bx bxs-cylinder' style='color:#ffffff' ></i>Achats</a></li>
-            <li><a href=""><i class='bx bxs-t-shirt'></i>Tous les articles</a></li>
-        </ul>
+        <div class="sidebar">
+            <div class="logo"><img src="img/sinmatexlogo.png" alt="simatex"></div>
+            <ul>
+                <li><a href=""><i class='bx bx-line-chart' style='color:#ffffff'></i> Dashboard</a></li>
+                <li><a href=""><i class='bx bxs-group' style='color:#ffffff'></i>Clients</a></li>
+                <li><a href=""><i class='bx bx-box'></i>Commandes</a></li>
+                <li><a href=""><i class='bx bxs-pie-chart-alt' style='color:#ffffff'></i>Fournisseurs</a></li>
+                <li><a href=""><i class='bx bxs-cylinder' style='color:#ffffff'></i>Achats</a></li>
+                <li><a href=""><i class='bx bxs-t-shirt'></i>Tous les articles</a></li>
+                <li><a href=""><i class='bx bx-log-out'></i>Déconnexion</a></li>
+            </ul>
+        </div>
     </div>
+
+    <section class="cartes">
+        <div class="card">
+            <h4>Commandes</h4>
+        </div>
+        <div class="card">
+            <h4>Clients</h4>
+        </div>
+        <div class="card">
+            <h4>Articles</h4>
+        </div>
+        <div class="card">
+            <h4>Commandes <br>non fini</h4>
+        </div>
+    </section>
+    <section>
+        <div class="client"></div>
+        <h4>Clients</h4>
+
+
+    </section>
 
 </body>
 
